3248696fe219fc76eb8424f91be421a13b06e4d1
kylegallard <kylegallard@gmail.com>
2020-05-27 12:04:37 +1000

    More set-up for routing. Added Posts.js container, removed some content from main output page Blog.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,58 +1,11 @@
 import React, { Component } from 'react';
-import axiosInstance from '../../axios';
 
-import Post from '../../components/Post/Post';
+// import Post from '../../components/Post/Post';
 import classes from './Blog.module.css';
-import Spinner from '../../components/Spinner/Spinner';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false,
-        mounted: false
-    }
-
-
-    componentDidMount() {
-        this.mounted = true;
-        axiosInstance.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Kyle',
-                    }
-                });
-                this.setState({ posts: updatedPosts });
-                // console.log(response);
-            })
-            .catch(error => {
-                //console.log(error);
-                this.setState({ error: true });
-            });
-    }
-
-    postClickedHandler = (id) => {
-        this.setState({ selectedPostId: id });
-    }
-
     render() {
-        let posts = <p style={{ textAlign: 'center' }}><Spinner />Loading...</p>;
-        if (this.mounted) {
-            posts = this.state.posts.map(post => {
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postClickedHandler(post.id)} />
-            });
-        };
-        if (this.state.error) {
-            posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
-        }
-
         return (
             <div className={classes.Blog}>
                 <header>
@@ -63,9 +16,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className={classes.Posts}>
-                    {posts}
-                </section>
+                <Posts />
             </div>
         );
     }
